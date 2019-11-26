b191ab948fa526deac72fdd6e15e1808e5b2e845
Aaron <aaronvlak@hotmail.nl>
2019-11-26 13:02:04 +0100

    front-end fix

---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,16 @@
+<?php
+?>
+<section class="jumbotron text-center">
+    <div class="container">
+        <h1 class="jumbotron-heading"><img id="hey" src="img/Logo1_groot.png" alt="Logo"></h1>
+        <p class="shoulders">
+            Wij zijn Barroc<span class="yellow">.</span> <span class="roboto">Intens</span>, Voor al uw vlammend heerlijke koffie.
+        <p class="shoulders">Geniet van de zuiverste koffie op de wereld.</p>
+        </p>
+        <p>
+             <button> <a href="#" class="white"> Ik ben klant </a> </button>
+             <button> <a href="#" class="white"> Ik ben werknemer </a> </button>
+        </p>
+    </div>
+</section></div></div></html>
+
---
+++ resources/views/layouts/partials/nav.blade.php
@@ -0,0 +1,47 @@
+<?php
+?>
+<div class="collapse bg-inverse" id="navbarHeader">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8 py-4">
+                <h4 class="text-white">Over ons</h4>
+                <p class="text-muted">Wij, de gebroeders Pullens, kwamen met het idee om in koffie te gaan handelen omdat
+                wij elke dag veel koffie drinken.
+                We zijn met elkaar in gesprek gegaan, en hebben toen een business-plan opgezet
+                    om de zo zuiverste koffie mogelijk te kunnen leveren binnen Nederland.
+                </p>
+            </div>
+            <div class="col-sm-4 py-4">
+                <h4 class="text-white">Contact</h4>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-white">volg ons op Twitter</a></li>
+                    <li><a href="#" class="text-white">volg ons op Facebook</a></li>
+                    <li><a href="#" class="text-white">Email ons</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="navbar-inverse bg-inverse">
+    <div class="container">
+        <div class="header">
+        <div class="logo">
+            <img id="logo" src="img/Logo6_groot.png" alt="Logo">
+        </div>
+
+        <div class="container d-flex justify-content-betIen">
+            <a href="#" class="navbar-brand"></a>
+            <button  class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            Over ons / Contact
+            </button>
+
+
+        </div>
+            <nav>
+                <a href="">example1</a>
+                <a href="">example2</a>
+                <a href="">example3</a>
+            </nav>
+        </div>
+
